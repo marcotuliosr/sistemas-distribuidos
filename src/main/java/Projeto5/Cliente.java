30e74334a9c7ce30d97aa5fbac8a8fb81ca61a09
marcotuliosr <marcotuliopessoal@gmail.com>
2022-09-02 21:27:29 -0300

    WIP - PJ 1/2 - Concluido 3/4/5

---
+++ src/main/java/Projeto5/Cliente.java
@@ -0,0 +1,55 @@
+/**
+ * Created by marcotuliosr
+ * Date : 9/2/2022}
+ * Time : 9:47 AM
+ * Project Name : sistemas-distribuidos
+ */
+
+package Projeto5;
+
+import java.io.*;
+import java.net.*;
+
+public class Cliente {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket s = new DatagramSocket();
+            InetAddress dest = InetAddress.getByName("localhost");
+
+            String envio;
+
+            BufferedReader teclado = new BufferedReader(new
+
+                    InputStreamReader(System.in));
+
+            System.out.print("> ");
+            envio = teclado.readLine();
+
+            while (!envio.equalsIgnoreCase("")) {
+                byte[] buffer = envio.getBytes();
+
+                DatagramPacket msg = new DatagramPacket(buffer, buffer.length, dest, 4545);
+                s.send(msg);
+                DatagramPacket resposta = new DatagramPacket(new byte[512], 512);
+                s.receive(resposta);
+
+                for (int i = 0; i < resposta.getLength(); i++) {
+                    System.out.print((char) resposta.getData()[i]);
+                }
+                System.out.println();
+                System.out.print("> ");
+                envio = teclado.readLine();
+            }
+
+            s.close();
+
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
---
+++ src/main/java/Projeto5/Servidor.java
@@ -0,0 +1,39 @@
+/**
+ * Created by marcotuliosr
+ * Date : 9/2/2022}
+ * Time : 9:47 AM
+ * Project Name : sistemas-distribuidos
+ */
+
+package Projeto5;
+
+import java.io.*;
+import java.net.*;
+
+public class Servidor {
+    public static void main(String[] args) {
+        try {
+            DatagramSocket s = new DatagramSocket(4545);
+            System.out.println("Servidor esperando conexão.......");
+            String envio;
+            DatagramPacket recebe = new DatagramPacket(new byte[512], 512);
+            while (true) {
+                envio = "";
+                s.receive(recebe);
+                System.out.print("Mensagem recebida: ");
+                for (int i = 0; i < recebe.getLength(); i++) {
+                    System.out.print((char) recebe.getData()[i]);
+                }
+                System.out.println();
+                DatagramPacket resp = new DatagramPacket(recebe.getData(),
+                        recebe.getLength(), recebe.getAddress(), recebe.getPort());
+                s.send(resp);
+            }
+
+        } catch (
+                IOException e) {
+            throw new RuntimeException(e);
+
+        }
+    }
+}
