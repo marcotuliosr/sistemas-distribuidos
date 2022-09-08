bb1bde24861c3ab9199136cdd9b77e5f8b27eefe
marcotuliosr <marcotuliopessoal@gmail.com>
2022-09-08 17:35:25 -0300

    update

--- src/main/java/Projeto5/Servidor.java
+++ src/main/java/Projeto5/Servidor.java
@@ -32,7 +32,7 @@ public class Servidor {
 
         } catch (
                 IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e); //
 
         }
     }
